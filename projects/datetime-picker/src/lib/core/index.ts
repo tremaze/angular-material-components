fa652e177ee2fc736bd69502167c68afabc98057
Ruben <rubencidlara@gmail.com>
2024-06-10 11:07:49 +0200

    chore: Update datetime-picker component imports and providers

---
+++ projects/datetime-picker/src/lib/core/index.ts
@@ -0,0 +1,12 @@
+import { NgxMatDateAdapter } from './date-adapter';
+import { NGX_MAT_DATE_FORMATS } from './date-formats';
+import { NgxMatNativeDateAdapter } from './native-date-adapter';
+import { NGX_MAT_NATIVE_DATE_FORMATS } from './native-date-formats';
+
+export * from './native-date.module';
+export {
+  NGX_MAT_DATE_FORMATS,
+  NGX_MAT_NATIVE_DATE_FORMATS,
+  NgxMatDateAdapter,
+  NgxMatNativeDateAdapter,
+};
